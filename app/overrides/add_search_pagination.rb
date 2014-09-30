4fcbbad212966d44f4ba8b1a0ece36aab0d471c0
Marko Dolenc <dmarko@dev.support.si>
2014-09-30 11:38:10 +0200

    Bug fix

--- app/overrides/add_search_pagination.rb
+++ app/overrides/add_search_pagination.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/shared/_products",
                       :name => "add_sunspot_search_pagination",
-                      :replace => "code[erb-silent]:contains('if paginated_products.respond_to')",
-                      :closing_selector => "code[erb-silent]:contains('end')",
+                      :replace => "erb[silent]:contains('if paginated_products.respond_to')",
+                      :closing_selector => "erb[silent]:contains('end')",
                       :text => "<%= paginate @searcher.sunspot.hits %>")
